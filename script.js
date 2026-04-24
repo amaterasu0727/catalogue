db6f2730172093b22424261f10834c5c9fb83bbe
amaterasu0727 <vladimirfaton@gmail.com>
2026-04-24 09:42:34 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -282,7 +282,7 @@ const produits = [
         id: 32,
         nom: "Casque Sans Fil P9 Premium Design",
         prix: "3 500",
-        image: "casquep9.jpg",
+        image: "casqueP9.jpg",
         tags: "électronique, audio, musique, casque, luxe",
         description: "L'élégance au service du son. Un design épuré et une connexion Bluetooth rapide pour un usage quotidien fluide.",
         dispo: true
@@ -291,7 +291,7 @@ const produits = [
         id: 33,
         nom: "Casque Bluetooth P39 Color Edition",
         prix: "4 000",
-        image: "casquep39.jpg",
+        image: "casqueP39.jpg",
         tags: "électronique, audio, musique, casque, mode",
         description: "Affichez votre style en musique. Des couleurs vives et un confort d'écoute idéal pour les transports ou le sport.",
         dispo: true
